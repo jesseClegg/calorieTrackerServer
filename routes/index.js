b91460bc2c19606fc39d79f34d85401ea8a551e2
jesseClegg <89627047+jesseClegg@users.noreply.github.com>
2022-11-15 00:48:07 -0500

    successfully added existance checking to addUser and deleteUser

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
-
 var router = express.Router();
-const fs = require('fs');
-const Joi = require('joi');
 const cors = require('cors');
 
-//an array of course objects with 2 attributes each
-const foods = [
-  {id: 1, name: 'pizza', calories:500},
-  {id: 2, name: 'salad', calories: 100},
-  {id: 3, name: 'ice cream', calories: 700},
-  {id: 4, name: 'apples', calories: 70},
-];
+////////////////////////////////// MONGO DB OVERHEAD /////////////////////////////////////////////////
+const mongoose = require('mongoose')
+const User = require("./userSchemaMongo") //import that model created in userSchemaMongo.js
+
+mongoose.connect("mongodb+srv://jesseC:mongo4999@cluster0.kva1ucs.mongodb.net/?retryWrites=true&w=majority",
+    ()=> {
+      console.log("connected succesz")
+    },
+    e=> console.error(e)
+)
+////////////////////////////////////////////////////////////////////////////////////END MONGO DB OVERHEAD
 
 const activities = [
   {id: 1, name: 'running', minuteDuration:15, caloriesPerMinute:11},
@@ -26,28 +27,13 @@ const days = [
   {date: 3, intake: 1900, burned: 700},
 ];
 
-////////////////////////////////// MONGO DB OVERHEAD /////////////////////////////////////////////////
-const mongoose = require('mongoose')
-const User = require("./userSchemaMongo") //import that model created in userSchemaMongo.js
 
 
-mongoose.connect("mongodb+srv://jesseC:mongo4999@cluster0.kva1ucs.mongodb.net/?retryWrites=true&w=majority",
-    ()=> {
-      console.log("connected succesz")
-    },
-    e=> console.error(e)
-)
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-////////////////////////////////// MONGO FUNCTIONS /////////////////////////////////////////////////
+////////////////////////////////// MONGO HELPER FUNCTIONS /////////////////////////////////////////////////
 async function getFoodsByEmail(userEmail){
   //get the user object
-  const userFound=await findUser(userEmail);
-  if(userFound!==-1){
+  const userFound=await getUserObjectByEmail(userEmail);
+  if(userFound!==false){
     console.log("food gucci");
     // for(let i=0; i<userFound.foods.length; i++){
     //     console.log(userFound.foods[i]);
@@ -59,25 +45,50 @@ async function getFoodsByEmail(userEmail){
   }
 }
 
-async function findUser(emailTofind) {
-  const userFound = await User.findOne({email: emailTofind});
+async function getUserObjectByEmail(email) {
+  const userFound = await User.findOne({email: email});
   if(!userFound){
-    console.log("doesnt exist");
-    return -1;
+    //console.log("doesnt exist");
+    return false;
   }else{
      //console.log(userFound);
     return userFound;
   }
 }
-/////////////////////////////////////////////////////////////////////////////////////////////
+
+async function checkIfUserExists(email){
+  const userFound = await User.findOne({email: email});
+  if (userFound) {
+    return true;
+  } else {
+    return false;
+  }
+}
+async function checkIfFoodExists(email, foodToFind){
+    const userFound = getUserObjectByEmail(email);
+    if(userFound!==false){
+      //todo: search for food
+    }else{
+      return false;
+    }
+}
+async function checkIfActivityExists(email, activityToFind){
+  //todo
+}
+async function checkIfDateExists(email, dateToFind){
+  //todo
+}
+//////////////////////////////////////////////////////////////////////////////////END MONGO HELPER FUNCTIONS
+
+
 
 ////////////////////////////////// GET ROUTE HANDLERS ///////////////////////////////////////
 
 
 router.get('/api/foods',cors(), (req, res) => {
   const emaiLString="mylesMotha";
-  findUser(emaiLString).then((user) => {
-    console.log(user);
+  getUserObjectByEmail(emaiLString).then((user) => {
+    //console.log(user);
     res.send(user.foods);
   });
 })
@@ -85,36 +96,21 @@ router.get('/api/foods',cors(), (req, res) => {
 
 router.get('/api/activities',cors(), (req, res) => {
   const emaiLString="mylesMotha";
-  findUser(emaiLString).then((user) => {
-    console.log(user);
+  getUserObjectByEmail(emaiLString).then((user) => {
+    //console.log(user);
     res.send(user.activities);
   });
 })
 
 router.get('/api/days',cors(), (req, res) => {
   const emaiLString="mylesMotha";
-  findUser(emaiLString).then((user) => {
-    console.log(user);
+  getUserObjectByEmail(emaiLString).then((user) => {
+    //console.log(user);
     res.send(user.days);
   });
 })
 
-
-////////////////////////////////// GET ROUTE HANDLERS ///////////////////////////////////////
-
-
-// //checks the activities array to find one with matching id, request was made as a string so parseInt
-// router.get('/api/activities/:id',cors(), (req, res) =>{
-//   const activity=activities.find(c=> c.id === parseInt(req.params.id));
-//   //if(!course) res.status(404).send('404!!! course with given id was not found'); //404 means object not found
-//   res.send(activity);
-// })
-//
-// router.get('/api/foods/:name', cors(), (req, res) => {
-//   const food=foods.find(req.params.name);
-//   res.send(food);
-// })
-
+/////////////////////////////////////////////////////////////////////////END GET ROUTE HANDLERS
 
 
 
@@ -122,56 +118,15 @@ router.get('/api/days',cors(), (req, res) => {
 ////////////////////////////////// POST ROUTE HANDLERS ///////////////////////////////////////
 
 
-async function updateFood(userEmail, foodToFindName, newFoodObject){
-  const userFound=await findUser(userEmail);
-  if(userFound!==-1){
-    for(let i=0; i<userFound.foods.length; i++){
-      console.log(userFound.foods[i]);
-      if(userFound.foods[i].name===foodToFindName){
-        console.log("FOOD MATCHES!!")
-        userFound.foods[i]=newFoodObject;
-        await userFound.save();
-        console.log("update food gucci");
-        return true;
-      }
-    }
-  }else{
-    console.log("update food NOT gucci");
-    return false;
-  }
-}
-
-//updates an existing food
-router.put('/api/foods', (req, res)=> {
-  /// JUNK TEST DATA /////
-  const emaiLString="bob";
-  const foodToFind="willysNewFood"
-  const newFoodInfo = {
-   name: "foodYouChanged",
-    calories: 8989898
-  };
-  /////////////////////////
-  if(updateFood(emaiLString, foodToFind, newFoodInfo)){
-    const ansString ="successfully posted: "
-    console.log(ansString+newFoodInfo);
-    res.send(true);
-  }else {
-    console.log("failed to post food update")
-    res.send(false);
-  }
-});
-
-
-
-//insert a new food
+//INSERT A NEW FOOD  /////////////////////////////////
 router.post('/api/foods', (req, res)=> {
-  /// JUNK TEST DATA /////
-  const emaiLString="bob";
+
+  const emaiLString="bob"; /// JUNK TEST DATA
   const newFoodToInsert = {
     name: "willysNewFood",
     calories: 499
   };
-  /////////////////////////
+
   if(insertFood(emaiLString, newFoodToInsert)){
     const ansString ="successfully posted: "
     console.log(ansString+newFoodToInsert);
@@ -183,7 +138,7 @@ router.post('/api/foods', (req, res)=> {
 });
 
 async function insertFood(userEmail, newFoodObject){
-  const userFound=await findUser(userEmail);
+  const userFound=await getUserObjectByEmail(userEmail);
   if(userFound!==-1){
     for(let i=0; i<userFound.foods.length; i++){
       console.log(userFound.foods[i]);
@@ -206,10 +161,47 @@ async function insertFood(userEmail, newFoodObject){
     return false;
   }
 }
+/////////////////////////////////
 
 
+// ADD A NEW USER /////////////////////////////////
+router.post('/api/addUser', async (req, res) => {
+  const userEmailToAdd = req.body.email;
+  const userExists = await checkIfUserExists(userEmailToAdd);
+  if (!userExists) {
+    createNewUser(userEmailToAdd).then(r => {
+      res.send("successfully added user: " + userEmailToAdd);
+    });
+  } else {
+    res.send("user already exists");
+  }
 
 
+});
+
+async function createNewUser(emailToUse) {
+  let today = new Date().toISOString().slice(0, 10)
+  const user = await User.create(
+      {
+        email: emailToUse,
+        activities:[
+          {name: 'running', calories: 100},
+          {name: 'napping', calories: 19},
+          {name: 'swimming', calories: 300}
+        ],
+        days:[
+          {Day: today, caloriesIn: 9001, caloriesOut: 1},
+          {Day: today, caloriesIn: 9002, caloriesOut: 2}
+        ],
+        foods:[
+          {name: 'apples', calories: 70},
+          {name: 'pineapples', calories: 65},
+          {name: 'pie', calories: 400},
+        ]
+      }
+  )
+}
+/////////////////////////////////
 
 
 router.post('/api/activities', (req, res)=> {
@@ -233,73 +225,61 @@ router.post('/api/days', (req, res)=> {
   res.send(day);
 });
 
-////////////////////////////////// POST ROUTE HANDLERS ///////////////////////////////////////
-router.post('/api/addUser', (req, res)=> {
-  //TODO: MUST CHECK IF USER ALREADY EXISTS BEFORE ADDING!!!!
-  const userEmailToAdd= "spongeRobert";
-  createNewUser(userEmailToAdd).then(r => {
-    res.send("successfully added user: "+userEmailToAdd);
-  });
+/////////////////////////////////////////////////////////////////////////END POST ROUTE HANDLERS
 
-});
 
-async function createNewUser(emailToUse) {
-  let today = new Date().toISOString().slice(0, 10)
-  //console.log(today)
-  const user = await User.create(
-      {
-        email: emailToUse,
-        activities:[
-          {name: 'running', calories: 100},
-          {name: 'napping', calories: 19},
-          {name: 'swimming', calories: 300}
-        ],
-        days:[
-          {Day: today, caloriesIn: 9001, caloriesOut: 1},
-          {Day: today, caloriesIn: 9002, caloriesOut: 2}
-        ],
-        foods:[
-          {name: 'apples', calories: 70},
-          {name: 'pineapples', calories: 65},
-          {name: 'pie', calories: 400},
-        ]
-      }
-  )
-  //const user = new User({name: "jesse", age: 99}) //both do the same things
-  //user.name="worker number 7"
-  //await user.save()
-  //console.log(user)
-}
 
-// app.post('/api/food/makeFile', (req, res)=>{
-//   fs.appendFile('oopsies.txt', 'Hello pizz!', function (err) {
-//     if (err) throw err;
-//     console.log('Saved!');
-//   });
-// });
 
 ////////////////////////////////// PUT ROUTE HANDLERS ///////////////////////////////////////
+
+//UPDATE AN EXISTING FOOD  /////////////////////////////////
+router.put('/api/foods', (req, res)=> {
+  /// JUNK TEST DATA /////
+  const emaiLString="bob";
+  const foodToFind="willysNewFood"
+  const newFoodInfo = {
+    name: "foodYouChanged",
+    calories: 8989898
+  };
+  /////////////////////////
+  if(updateFood(emaiLString, foodToFind, newFoodInfo)){
+    const ansString ="successfully posted: "
+    console.log(ansString+newFoodInfo);
+    res.send(true);
+  }else {
+    console.log("failed to post food update")
+    res.send(false);
+  }
+});
+
+async function updateFood(userEmail, foodToFindName, newFoodObject){
+  const userFound=await getUserObjectByEmail(userEmail);
+  if(userFound!==-1){
+    for(let i=0; i<userFound.foods.length; i++){
+      console.log(userFound.foods[i]);
+      if(userFound.foods[i].name===foodToFindName){
+        console.log("FOOD MATCHES!!")
+        userFound.foods[i]=newFoodObject;
+        await userFound.save();
+        console.log("update food gucci");
+        return true;
+      }
+    }
+  }else{
+    console.log("update food NOT gucci");
+    return false;
+  }
+}
+/////////////////////////////////
+
 
 // router.put('/api/activities/:id', (req, res) =>{
 //   //console.log(req.params.id);
 //   const course=activities.find(c=> c.id === parseInt(req.params.id));
-//   if(!course) res.status(404).send('404!!!  id was not found'); //404 means object not found
 //
-//   //validate the course,
-//   //if invalid, return 400 = Bad request
-//
-//  // const result = validateCourse(req.body);
-//
-//   //"object destructuring"
-//   //const {error} = validateCourse(req.body);
-//   // if(error) {
-//   //   res.status(400).send(`put oopsie ${req.params.id}`);
-//   // }
-//
-//   //update course
 //   course.name = req.body.name;
 //
-//   //return updated course to the client
+//
 //   res.send(course.name);
 // });
 
@@ -310,47 +290,33 @@ async function createNewUser(emailToUse) {
 
 /////////////////////////// DELETE ROUTE HANDLERS ////////////////////////////////////
 
-router.delete('/api/deleteUser', (req, res) => {
-  const emaiLString="spongeRobert";
-  deleteByEmail(emaiLString).then(r => {
-    res.send("successfully deleted: "+emaiLString);
-  });
+router.delete('/api/deleteUser', async (req, res) => {
+  const emailToDelete = req.body.email;
+  const userExists = await checkIfUserExists(emailToDelete);
+  if (userExists) {
+    deleteByEmail(emailToDelete).then(r => {
+      res.send("successfully deleted: " + emailToDelete);
+    });
+  } else {
+    res.send("failed to delete: " + emailToDelete+" does not exist");
+  }
+
+
 });
 
 async function deleteByEmail(emailToDelete) {
   await User.deleteOne({email: emailToDelete}, function (err, docs){
     if (err){
-      console.log(err)
+      //console.log(err)
     }
     else{
-      console.log("Deleted User : ", docs);
+      //console.log("Deleted User : ", docs);
     }
   }).clone();
 
 }
 
-// router.delete('/api/activities/:id', (req, res) => {
-//   //look up course
-//   const course=activities.find(c=> c.id === parseInt(req.params.id));
-//   //if not there => return 404
-//   if(!course) res.status(404).send('404!!!  id was not found'); //404 means object not found
-//
-//   //else delete it
-//   const index = activities.indexOf(course);
-//   activities.splice(index, 1);//seems like there's 10 other ways to delete this object, here's one way
-//
-//   //return the course that was deleted to client
-//   res.send(course);
-// });
-//
-//
-//
-// function validateCourse(course){
-//   const schema ={
-//     name: Joi.string().min(3).required()
-//   };
-//   return result= Joi.valid(course, schema);
-// }
 
+/////////////////////////////////////////////////////////////////////////END DELETE ROUTE HANDLERS
 
 module.exports = router;
